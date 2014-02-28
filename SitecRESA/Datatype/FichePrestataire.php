cc50187b9d809b93f791c2fa46ce26262640be79
MarcFRICOU <marc.fricou@sitec.fr>
2014-02-28 09:35:02 +0100

    add static lastModified method on FichePrestataire. Multiple correction on phpdoc

--- SitecRESA/Datatype/FichePrestataire.php
+++ SitecRESA/Datatype/FichePrestataire.php
@@ -2,22 +2,23 @@
 
 
 namespace SitecRESA\Datatype;
+use SitecRESA\WS\Client;
 
 /**
  * Fiche descriptive d'un hôtel ou de tout autre prestataire SitecRESA.
  *
  * @author Marc FRICOU <marc.fricou@sitec.fr>
- * 
+ *
  * @property-read int $id identifiant de la fiche
  * @property-read string $raisonSociale nom commercial du prestataire
  * @property-read string $siteWeb url (avec HTTP) d'accès au site web du prestataire.
  * @property-read string $valueNbEtoile Traduction française du classement du prestaire (s'il existe)
  * @property-read string $description Texte français de description du prestaire
  * @property-read float $taxeSejour Taxe de séjour demandée par nuit et par personne
- * @property-read \SitecRESA\Photo $photo 1ere photo du prestataire
- * @property-read \SitecRESA\Adresse $adresse adresse du prestataire
- * @property-read \SitecRESA\PositionGPS $position du prestataire.
- * @property-read \SitecRESA\Cgv $cgv conditions générales de ventes du prestataire (nécessite un accès WS supplémentaire.)
+ * @property-read \SitecRESA\Datatype\Photo $photo 1ere photo du prestataire
+ * @property-read \SitecRESA\Datatype\Adresse $adresse adresse du prestataire
+ * @property-read \SitecRESA\Datatype\PositionGPS $position du prestataire.
+ * @property-read \SitecRESA\Datatype\Cgv $cgv conditions générales de ventes du prestataire (nécessite un accès WS supplémentaire.)
  * @property-read array $galleriePhoto liste de toutes les photos (SitecRESA_Photo) du prestataire (dont $this->photo) (nécessite un accès WS supplémentaire.)
  * @property-read array $equipements liste de tous les équipements (SitecRESA_Equipement) du prestataire. (nécessite un accès WS supplémentaire.)
  * @property-read array $fichesPrestation liste des prestations du prestataire (les chambres d'un hôtel, par exemple). (nécessite un accès WS supplémentaire.)
@@ -43,32 +44,30 @@ class FichePrestataire extends DatatypeAbstract implements Fetchable{
     protected $_fichesPrestation;
     protected $_positionGPS;
     /**
-     *
      * @var AccesResolver
      */
     protected $_accesCalendrier;
     /**
-     *
      * @var AccesResolver
      */
     protected $_accesPrixPlancher;
-    
+
     /**
-     *
      * @var AccesResolver
      */
     protected $_accesPrestation;
-    
+
     public function __construct($apiClient, $array = NULL) {
         parent::__construct($apiClient, $array);
         $this->_accesPrestations = $this->_fichesPrestation;
     }
-    
+
     /**
      * prix plancher des prestations proposées (disponibles) pour une recherche de séjour donnée
-     * @param string|Zend_Date $dateArrivee
-     * @param string|Zend_Date $dateDepart
-     * @param int $iNbJours
+     *
+     * @param string $dateArrivee
+     * @param string $dateDepart
+     *
      * @return PrixPlancher
      */
     public function prixPlancher($dateArrivee,$dateDepart) {
@@ -76,12 +75,14 @@ class FichePrestataire extends DatatypeAbstract implements Fetchable{
             'dateFin' => $dateDepart,
             'dateDebut' => $dateArrivee));
     }
-    
+
     /**
-     * Permet d’obtenir les disponibilités sur plusieurs semaines du prestataires (compilation des disponibilités des chambres de l’prestataire)
-     * @param string|Zend_Date $dateArrivee
-     * @param string|Zend_Date $dateDepart
-     * @param int $iNbJours
+     * Permet d’obtenir les disponibilités sur plusieurs semaines du prestataires
+     * (compilation des disponibilités des chambres de l’prestataire)
+     *
+     * @param string $dateArrivee
+     * @param string $dateDepart
+     *
      * @return array
      */
     public function calendrierDispo($dateArrivee,$dateDepart) {
@@ -89,13 +90,14 @@ class FichePrestataire extends DatatypeAbstract implements Fetchable{
             'dateFin' => $dateDepart,
             'dateDebut' => $dateArrivee));
     }
-    
+
     /**
      * obtenir uniquement les prestations dispo
-     * @param string|Zend_Date $dateArrivee
-     * @param type $dateDepart
-     * @param type $avecTarif
-     * 
+     *
+     * @param string  $dateArrivee
+     * @param string  $dateDepart
+     * @param boolean $avecTarif
+     *
      * @return array liste de FichePrestation
      */
     public function prestationsDisponibles ($dateArrivee,$dateDepart, $avecTarif = FALSE) {
@@ -107,69 +109,84 @@ class FichePrestataire extends DatatypeAbstract implements Fetchable{
             )
         );
     }
-    
+
     /**
      * permet d'obtenir une liste de FichePrestataire avec lesquels vous avez un contrat.
-     * @return \SitecRESA\AccesResolverList
+     *
+     * @param Client $apiClient
+     *
+     * @return \SitecRESA\Datatype\AccesResolverList
      */
     static function listePrestataires($apiClient) {
         return $apiClient->listeorganismes("get",array());
     }
-    
+
     /**
-     * 
+     *
      * Permet d'obtenir la liste des prestataires disponibles aux dates fournies
-     * 
-     * @param SitecRESA\WS\Client $apiClient
-     * @param string $dateDebut format JJ/MM/AAAA
-     * @param string $dateFin format JJ/MM/AAAA
-     * @param array $aRepartition tableau d'entiers. Chaque entier correspond au nombre de personne pour une chambre
-     * @param int $nbChambre nombre de chambre, si la répartition n'est pas nécessaire
-     * @param int $nbPersonne nombre de personnes totale, si la répartition n'est pas nécessaire.
-     * @param array $regionVille liste des villes surlesquelles filtrer.
-     * @param type $avecTarif permet de préciser si les hôtels doivent être réservable (avec un tarif et des conditions adéquat : séjour min., etc.) ; TRUE par défaut
-     * @param string $orderBy permet de présiser un ordre : {@see FichePrestataire::ORDRE_COMMUNE}, {@see FichePrestataire::ORDRE_NBETOILE}, {@see FichePrestataire::ORDRE_NOM}
-     * @param int $count pour faire une pagination
-     * @param int $offset pour faire une pagination
-     * 
-     * @return \SitecRESA\AccesResolverList
+     *
+     * @param Client        $apiClient
+     * @param string        $dateArrivee format JJ/MM/AAAA
+     * @param string        $dateDepart format JJ/MM/AAAA
+     * @param int           $nbChambre nombre de chambre, si la répartition n'est pas nécessaire
+     * @param int           $nbPersonne nombre de personnes totale, si la répartition n'est pas nécessaire.
+     * @param array         $aRepartition tableau d'entiers. Chaque entier correspond au nombre de personne pour une chambre
+     * @param array|string  $regionVille liste des villes surlesquelles filtrer.
+     * @param boolean       $avecTarif permet de préciser si les hôtels doivent être réservable (avec un tarif et des conditions adéquat : séjour min., etc.) ; TRUE par défaut
+     * @param string        $orderBy permet de présiser un ordre : {@see FichePrestataire::ORDRE_COMMUNE}, {@see FichePrestataire::ORDRE_NBETOILE}, {@see FichePrestataire::ORDRE_NOM}
+     * @param int           $count pour faire une pagination
+     * @param int           $offset pour faire une pagination
+     *
+     * @return \SitecRESA\Datatype\AccesResolverList
      */
-    static function listePrestatairesDisponibles($apiClient,$dateArrivee, $dateDepart, 
+    static function listePrestatairesDisponibles($apiClient, $dateArrivee, $dateDepart,
             $nbChambre = 1, $nbPersonne = 2, $aRepartition = NULL,
-            $regionVille = self::REGIONVILLE_WILDCARD, 
-            $avecTarif = TRUE, 
+            $regionVille = self::REGIONVILLE_WILDCARD,
+            $avecTarif = TRUE,
             $orderBy = NULL, $count = NULL, $offset = NULL) {
-        
+
         $params = array(
-            "dateDebut"   => $dateArrivee, 
+            "dateDebut"   => $dateArrivee,
             "dateFin"     => $dateDepart,
-            "nbChambre"   => $nbChambre, 
-            "nbPersonne"  => $nbPersonne, 
+            "nbChambre"   => $nbChambre,
+            "nbPersonne"  => $nbPersonne,
             "repartition" => \Zend_Json::encode($aRepartition),
-            "regionVille" => \Zend_Json::encode($regionVille), 
+            "regionVille" => \Zend_Json::encode($regionVille),
             "avecTarif"   => $avecTarif,
             "orderBy"     => $orderBy,
             "count"       => $count,
             "offset"      => $offset
         );
-        
+
         if(!$orderBy){
             global $apiConfig;
             $params["orderBy"] = $apiConfig["triDefault"];
         }
-        
+
         return $apiClient->dispoorganismes("get", $params);
     }
 
     /**
-     * 
-     * @param \SitecRESA\WS\Client $apiClient
-     * @param int $id
+     * @param Client $apiClient
+     * @param int    $id
      * @return FichePrestataire
      */
-    static function fetch(\SitecRESA\WS\Client $apiClient, $id) {
+    static function fetch(Client $apiClient, $id) {
         return $apiClient->organisme("get",array("idRessource" => $id));
     }
-    
+
+    /**
+     * Get last modified date
+     *
+     * @param Client $apiClient
+     * @param int    $id
+     *
+     * @return string AAAA-MM-JJ HH:mm:SS
+     */
+    static public function lastModified(Client $apiClient, $id)
+    {
+        $fiche = $apiClient->propertylastmodified("get",array("idRessource" => $id));
+        return $fiche->lastModified;
+    }
 }
 

a34a94a662c6116cc9255baa5ee4212ac54ac43c
patricebrun <patrice.brun@sitec.fr>
2015-08-07 10:35:56 +0200

    Ajout idPrestation dans DisponibiliteProduit

--- SitecRESA/Datatype/DisponibiliteProduit.php
+++ SitecRESA/Datatype/DisponibiliteProduit.php
@@ -11,12 +11,14 @@ namespace SitecRESA\Datatype;
  * @property-read array $plansTarifaires liste de plans tarifaires
  * @property-read string $dureeFormatee durée au format fr en jours,heures et minutes
  * @property-read string $duree durée en minutes
+ * @property-read string $idPrestation
  */
 class DisponibiliteProduit extends DatatypeAbstract{
     protected $_quantiteDispo;
     protected $_plansTarifaires;
     protected $_dureeFormatee;
     protected $_duree;
+    protected $_idPrestation;
 
 }
 

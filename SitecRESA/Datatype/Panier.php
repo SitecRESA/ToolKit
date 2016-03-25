f17e4425e8be821213b59da2cf4d142cfb3defe0
Dam2a <damien.cayzac@sitec.fr>
2016-03-25 17:36:22 +0100

    Correctif bug multiPaiement
    Correctif bug multiPaiement

--- SitecRESA/Datatype/Panier.php
+++ SitecRESA/Datatype/Panier.php
@@ -200,7 +200,7 @@ class Panier extends SavableDatatypeAbstract implements Fetchable{
 
                     $i++;
                     if($oGarantie->unite == "%"){
-                        $montant = ($prestationPanier->tarif * $oGarantie->valeur / 100) - $montantPreleve;
+                        $montant = ($prestationPanier->tarif->prix * $oGarantie->valeur / 100) - $montantPreleve;
                         $montantPreleve += $montant;
                     }
                     $paiement = new Paiement($montant,$dateValide);

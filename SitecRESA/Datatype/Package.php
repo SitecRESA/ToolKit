93bdad465321f58dbccd02df0e5e3808541f6ed0
patricebrun <patrice.brun@sitec.fr>
2016-06-20 10:35:28 +0200

    Ajout du datatype package

---
+++ SitecRESA/Datatype/Package.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * User: patriceb
+ * Date: 20/06/2016
+ * Time: 10:08
+ */
+
+namespace SitecRESA\Datatype;
+
+/**
+ * Panier permettant de faire une réservation et de gérer des prestations et plusieurs séjours
+ *
+ * @author Patrice BRUN <patrice.brun@sitec.fr>
+ *
+ * @property int    $id identifiant unique
+ * @property string $libelle nom
+ * @property string $description description
+ * @property float  $prix prix
+ * @property int    $jourArrivee jour d'arrivée 0|1|2|3|4|5|6
+ * @property string $themes
+ * @property array  $etapes liste des étapes
+
+ */
+
+class Package extends SavableDatatypeAbstract implements Fetchable{
+
+    protected $_id;
+    protected $_libelle;
+    protected $_description;
+    protected $_prix;
+    protected $_jourArrivee;
+    protected $_themes;
+    protected $_etapes;
+    /**
+     *
+     * @param  \SitecRESA\WS\Client $apiClient
+     * @param  int $id
+     * @return self
+     */
+    public static function fetch(\SitecRESA\WS\Client $apiClient, $id) {
+        return $apiClient->package("get",array("idRessource"=> $id));
+    }
+
+    public function save() {
+
+    }
+
+    public function toArray() {
+
+    }
+
+}

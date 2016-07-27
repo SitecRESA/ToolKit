1ff212a1f5b86fa60334a210b73ef1e634b79024
damien.cayzac@sitec.fr <dam2A+$1108>
2016-07-27 17:20:21 +0200

    - ajout attribut dateArrivee et dateDepart si recherche de dispo

--- SitecRESA/Datatype/ProduitEtape.php
+++ SitecRESA/Datatype/ProduitEtape.php
@@ -18,6 +18,8 @@ namespace SitecRESA\Datatype;
  * @property int              $jourSession jour de la semaine [0..6]
  * @property string           $heureDebut heure de debut
  * @property bool             $dispo le produit est disponible ou non
+ * @property string      $dateArrivee  format d/m/Y si une recherche de dispo sur le package est effectuée
+ * @property string      $dateDepart  format d/m/Y si une recherche de dispo sur le package est effectuée
  */
 
 class ProduitEtape extends DatatypeAbstract implements Fetchable{
@@ -31,6 +33,8 @@ class ProduitEtape extends DatatypeAbstract implements Fetchable{
     protected $_heureDebut;
     protected $_categorie;
     protected $_dispo;
+    protected $_dateArrivee;
+    protected $_dateDepart;
 
     /**
      *

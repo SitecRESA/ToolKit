0f84b0bf8d157e0bac20423dcf4796541a115e25
damien.cayzac@sitec.fr <dam2A+$1108>
2016-08-08 16:51:35 +0200

    - ajout de l'attribut repartition

--- SitecRESA/Datatype/FichePrestataireEtape.php
+++ SitecRESA/Datatype/FichePrestataireEtape.php
@@ -5,10 +5,12 @@ namespace SitecRESA\Datatype;
  * @author Damien CAYZAC <damien.cayzac@sitec.fr>
  *
  * @property array           $produitsEtape
- * @property bool             $dispo le produit est disponible ou non
+ * @property array           $repartition
+ * @property bool            $dispo le produit est disponible ou non
  */
 
 class FichePrestataireEtape extends FichePrestataire {
     protected $_dispo;
     protected $_produitsEtape;
+    protected $_repartition;
 }

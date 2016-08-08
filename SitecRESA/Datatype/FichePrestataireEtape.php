76b265c1c43f1f6d94005887b6e0e3bfc7b11630
damien.cayzac@sitec.fr <dam2A+$1108>
2016-08-08 16:44:12 +0200

    - suppression methode getProduitsEtape

--- SitecRESA/Datatype/FichePrestataireEtape.php
+++ SitecRESA/Datatype/FichePrestataireEtape.php
@@ -4,28 +4,11 @@ namespace SitecRESA\Datatype;
 /**
  * @author Damien CAYZAC <damien.cayzac@sitec.fr>
  *
- * @property string           $produit libellé de lma fichePrestation
+ * @property array           $produitsEtape
  * @property bool             $dispo le produit est disponible ou non
  */
 
 class FichePrestataireEtape extends FichePrestataire {
     protected $_dispo;
-
-    /**
-     * Permet d'obtenir les produits etapes d'un contenu etape
-     *
-     * @param int idEtape
-     * @param int idContenuEtape
-     * @param string d/m/Y
-     *
-     * @return ObjectList
-     */
-    public function getProduitsEtape($idEtape,$idContenuEtape, $date = null) {
-        return $this->_apiClient->produitsetape("get",array(
-            'idRessource' => $this->id,
-            'idEtape' => $idEtape,
-            'idContenuEtape' => $idContenuEtape,
-            'dateDebut' => $date)
-        );
-    }
+    protected $_produitsEtape;
 }
